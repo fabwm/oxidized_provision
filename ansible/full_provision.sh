28d1265b2b5db4305157d0601ccfcaf462bd5aa0
fabwm <106402940+fabwm@users.noreply.github.com>
2022-10-24 16:21:23 -0300

    Update full_provision.sh

--- ansible/full_provision.sh
+++ ansible/full_provision.sh
@@ -1,12 +1,13 @@
 
 #!/bin/bash
-echo "Copiando chave ssh..."
+echo "Copying SSH public key..."
 scp $SSH_PUBLIC_KEY $ANSIBLE_USER@$ANSIBLE_HOST:/home/$ANSIBLE_USER/.ssh/uploaded_rsa.pub
 ansible oxidized -u $ANSIBLE_USER -i hosts -m shell -a 'cat ~/.ssh/uploaded_rsa.pub >> ~/.ssh/authorized_keys' -k
 ansible oxidized -u $ANSIBLE_USER -i hosts -m shell -a 'rm ~/.ssh/uploaded_rsa.pub'
-echo "Copiando arquivo de rota..."
+echo "Copying router file..."
 scp ../config/router.db $ANSIBLE_USER@$ANSIBLE_HOST:/home/$ANSIBLE_USER/
-echo "Copiando arquivo de configuração..."
+echo "Copying config file..."
 scp ../config/config $ANSIBLE_USER@$ANSIBLE_HOST:/home/$ANSIBLE_USER/
-echo "Servidor pronto para provisionamento"
+echo "Server will now be provised..."
 ansible-playbook oxidized_full_provision.yml -i hosts -u $ANSIBLE_USER --private-key /home/ansible/.ssh/id_rsa -v -b
+echo "DONE"
